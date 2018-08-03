6fc0b8b693c795cde2744f150140fa353b19cecf
Spencer Sansone <spencersemail101@gmail.com>
2018-08-03 22:01:03 +0000

    add steps to task_detail

--- main/views.py
+++ main/views.py
@@ -14,8 +14,10 @@ def tasks(request):
 
 def task_detail(request, pk):
     certain_task = Task.objects.get(id=pk)
+    certain_task_steps = Step.objects.filter(task=certain_task)
     x = {}
     x['certain_task'] = certain_task
+    x['certain_task_steps'] = certain_task_steps
     return render(request, 'main/task_detail.html', x)
 
 # Create your views here.
