0d78351e0aab8877418036c29a9c58eb9d18d0b0
Spencer Sansone <spencersemail101@gmail.com>
2018-08-04 04:20:51 +0000

    add add_step

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^task_list/$', views.task_list, name='task_list'),
     url(r'^add_task/$', views.add_task, name='add_task'),
+    url(r'^add_step/(?P<pk>[0-9]+)/$', views.add_step, name='add_step'),
     url(r'^task_detail/(?P<pk>[0-9]+)/$', views.task_detail, name='task_detail'),
     url(r'^delete_task/(?P<pk>[0-9]+)/$', views.delete_task, name='delete_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from .models import *
 import datetime
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def home(request):
     x = {}
@@ -28,6 +30,34 @@ def add_task(request):
         return redirect('main:task_list')
     else:
         return render(request, 'main/add_task.html')
+        
+def add_step(request, pk):
+    if request.method == "POST":
+        n = request.POST.get('name')
+        certain_task = Task.objects.get(id=pk)
+        certain_task_steps = Step.objects.filter(task=certain_task)
+        
+        
+        greatest_number = 0
+        for step in certain_task_steps:
+            if step.step_number > greatest_number:
+                greatest_number = step.step_number
+        
+        Step.objects.create(
+            name = n,
+            task = certain_task,
+            step_number = greatest_number + 1)
+        
+        x = {}
+        x['pk'] = pk
+        # return redirect('main:task_detail', kwargs=x)
+        return HttpResponseRedirect(reverse('main:task_detail', kwargs=x))
+
+
+    else:
+        x = {}
+        x['certain_task'] = Task.objects.get(id=pk)
+        return render(request, 'main/add_step.html', x)
         
 def delete_task(request, pk):
     if request.method == "POST":
