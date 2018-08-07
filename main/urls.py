e6356110c3047c9f1524dc8832c133b1fbf062a9
Spencer Sansone <spencersemail101@gmail.com>
2018-08-07 22:19:35 +0000

    add edit_task

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^task_list/$', views.task_list, name='task_list'),
     url(r'^today/$', views.today, name='today'),
     url(r'^add_task/$', views.add_task, name='add_task'),
+    url(r'^edit_task/(?P<pk>[0-9]+)/$', views.edit_task, name='edit_task'),
     url(r'^add_step/(?P<pk>[0-9]+)/$', views.add_step, name='add_step'),
     url(r'^task_detail/(?P<pk>[0-9]+)/$', views.task_detail, name='task_detail'),
     url(r'^start_new_task_entry/(?P<pk>[0-9]+)/$', views.start_new_task_entry, name='start_new_task_entry'),
--- main/views.py
+++ main/views.py
@@ -108,6 +108,49 @@ def task_detail(request, pk):
     x['certain_task'] = certain_task
     x['certain_task_steps'] = Step.objects.filter(task=certain_task)
     return render(request, 'main/task_detail.html', x)
+    
+def edit_task(request, pk):
+    certain_task = Task.objects.get(id=pk)
+    if request.method == "POST":
+        if "routine_option" in request.POST:
+            n = request.POST.get('name')
+            sun = True if request.POST.get('sunday') == "on" else False
+            mon = True if request.POST.get('monday') == "on" else False
+            tue = True if request.POST.get('tuesday') == "on" else False
+            wed = True if request.POST.get('wednesday') == "on" else False
+            thu = True if request.POST.get('thursday') == "on" else False
+            fri = True if request.POST.get('friday') == "on" else False
+            sat = True if request.POST.get('saturday') == "on" else False
+            
+            certain_task.sunday = sun
+            certain_task.monday = mon
+            certain_task.tuesday = tue
+            certain_task.wednesday = wed
+            certain_task.thursday = thu
+            certain_task.friday = fri
+            certain_task.saturday = sat
+            certain_task.name = n
+            certain_task.save()
+            x = {}
+            x['pk'] = pk
+            return HttpResponseRedirect(reverse('main:task_detail', kwargs=x))
+        elif "certain_due_date_option" in request.POST:
+            n = request.POST.get('name')
+            d = request.POST.get('certain_date')
+            
+            certain_task.name = n
+            certain_task.date = d
+            certain_task.save()
+            x = {}
+            x['pk'] = pk
+            return HttpResponseRedirect(reverse('main:task_detail', kwargs=x))
+            print("certain date")
+    else:
+        pass
+    
+    x = {}
+    x['certain_task'] = certain_task
+    return render(request, 'main/edit_task.html', x)
 
 @cache_control(must_revalidate=True, max_age=3600)
 def add_task(request):
